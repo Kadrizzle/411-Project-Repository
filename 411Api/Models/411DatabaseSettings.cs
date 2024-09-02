bc3f590597903beb2e55939c091aa72cf3997ae9
Kadrizzle <lanekade8@gmail.com>
2024-09-02 15:14:26 -0500

    Add project files.

---
+++ 411Api/Models/411DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace _411Api.Models
+{
+    public class _411DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string VapesCollectionName { get; set; } = null!;
+    }
+}
---
+++ 411Api/Models/Vape.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace _411Api.Models
+{
+    public class Vape
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; } = null!;
+        public string CompanyName { get; set; } = null!;
+        public string NicotineConcentration { get; set; } = null!;
+    }
+}
