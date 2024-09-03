939e2c2ca8dce7b2bcf2a1d877af762a9693f268
Kadrizzle <lanekade8@gmail.com>
2024-09-03 16:41:22 -0500

    VapesController,,, Will have to update some of the endpoints to match vape class variables

---
+++ 411Api/Controllers/VapesController.cs
@@ -0,0 +1,73 @@
+﻿using _411Api.Models;
+using _411Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _411Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VapesController : ControllerBase
+    {
+        private readonly VapesService _vapesService;
+
+        public VapesController(VapesService vapesService) =>
+            _vapesService = vapesService;
+
+        [HttpGet]
+        public async Task<List<Vape>> Get() =>
+            await _vapesService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Vape>> Get(string id)
+        {
+            var vape = await _vapesService.GetAsync(id);
+
+            if (vape is null)
+            {
+                return NotFound();
+            }
+
+            return vape;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Vape newVape)
+        {
+            await _vapesService.CreateAsync(newVape);
+
+            return CreatedAtAction(nameof(Get), new { id = newVape.Id }, newVape);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Vape updatedVape)
+        {
+            var vape = await _vapesService.GetAsync(id);
+
+            if (vape is null)
+            {
+                return NotFound();
+            }
+
+            updatedVape.Id = vape.Id;
+
+            await _vapesService.UpdateAsync(id, updatedVape);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var vape = await _vapesService.GetAsync(id);
+
+            if (vape is null)
+            {
+                return NotFound();
+            }
+
+            await _vapesService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
