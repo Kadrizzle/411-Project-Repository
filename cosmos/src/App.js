0eac441ae2217209ac5435b8a58e22322422a877
Kade Lane <98795639+Kadrizzle@users.noreply.github.com>
2024-10-02 16:20:59 -0500

    Merge pull request #4 from Kadrizzle/react-branch
    user login page
    # Conflicts:
    #	cosmos/src/App.js

--- cosmos/src/App.js
+++ cosmos/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Home />
     </div>
   );
 };
--- cosmos/src/components/Navbar.js
+++ cosmos/src/components/Navbar.js
@@ -1,34 +1,81 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import About from "./About";
+import Login from "./Login";
+import Home from "./Home";
+import UserInfo from "./UserInfo";
+import CalendarView from "./CalendarView"; // Updated from Navbar
+import GraphView from "./GraphView"; // Updated from Navbar
 
 const Navbar = () => {
+  const [data, setData] = useState({
+    Monday: 0,
+    Tuesday: 0,
+    Wednesday: 0,
+    Thursday: 0,
+    Friday: 0,
+    Saturday: 0,
+    Sunday: 0,
+  });
+
   return (
-    <nav className="navbar">
-      <div className="container-fluid">
-        <div className="navbar-collapse d-flex justify-content-between align-items-center">
-          {" "}
-          {/* Added justify-content-between */}
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About Us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/services">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <img src="/COSMOS-purple.png" alt="COSMOS Logo" className="navbar-logo" />
+    <>
+      <nav className="navbar">
+        <div className="container-fluid">
+          <div className="navbar-collapse d-flex justify-content-between align-items-center">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/userInfo">
+                  User Info
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/services">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <img src="/COSMOS-purple.png" alt="COSMOS Logo" className="navbar-logo" />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/userInfo"
+          element={
+            <div>
+              <UserInfo data={data} setData={setData} />
+              <CalendarView />
+              <GraphView data={data} />
+            </div>
+          }
+        />
+      </Routes>
+    </>
   );
 };
 
