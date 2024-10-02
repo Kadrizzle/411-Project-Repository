333dd6df4821b2664e27cc9af46902db4745cd96
Kadrizzle <lanekade8@gmail.com>
2024-10-02 16:11:14 -0500

    Commenting out reference to User class in VapeMoneyStatistic class (Was causing issues)

--- 411Api/Models/VapeMoneyStatistic.cs
+++ 411Api/Models/VapeMoneyStatistic.cs
@@ -10,9 +10,9 @@ namespace _411Api.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public int MoneySpentOnVape { get; set; }
-        public DateTime DateVapeWasBought { get; set; } 
+        public DateTime DateVapeWasBought { get; set; }
         public string? UserId { get; set; }
-        public User User { get; set; } = null!;
-
+        //Commenting out reference to user class because I dont think we'll need it for our mongodb database
+        //public User User { get; set; } = null!;
     }
 }
